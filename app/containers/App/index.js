0864a9ec1dc6f834ca0d51581077e0a7220696b5
Paige Godfrey <paagodfrey@gmail.com>
2020-05-29 18:18:16 -0700

    started header

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,21 +8,39 @@
  */
 
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
+import AddString from 'containers/AddString/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Header from 'components/Header';
 
 import GlobalStyle from '../../global-styles';
 
+const AppWrapper = styled.div`
+  max-width: calc(768px + 16px * 2);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100%;
+  padding: 0 16px;
+  flex-direction: column;
+`;
+
 export default function App() {
   return (
-    <div>
+    <AppWrapper>
+      <Helmet>
+        <meta name="description" content="A React.js Boilerplate application" />
+      </Helmet>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
+        <Route path="/create" component={AddString} />
+        <Route path="" component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
-    </div>
+    </AppWrapper>
   );
 }
